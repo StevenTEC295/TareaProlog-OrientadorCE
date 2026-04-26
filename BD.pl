6fd9556af0c01f1f5a5525b56f5925d1e1cad397
StevenProgrammer01 <s.perez.3@estudiantec.cr>
2026-04-26 00:12:58 -0600

    Se genero la Base de Datos del Sistema Experto en Prolog

---
+++ BD.pl
@@ -0,0 +1,169 @@
+profesion(medicina).
+profesion(ingenieria_software).
+profesion(derecho).
+profesion(psicologia).
+profesion(arquitectura).
+profesion(administracion_empresas).
+profesion(ensenanza).
+profesion(periodismo).
+profesion(diseno_grafico).
+profesion(biologia).
+profesion(contabilidad).
+profesion(trabajo_social).
+
+afinidad(medicina, ciencias_naturales).
+afinidad(medicina, biologia).
+afinidad(medicina, quimica).
+afinidad(medicina, empatia).
+afinidad(medicina, ayudar_personas).
+afinidad(medicina, pensamiento_analitico).
+afinidad(medicina, tolerancia_presion).
+afinidad(medicina, trabajo_en_equipo).
+
+afinidad(ingenieria_software, matematicas).
+afinidad(ingenieria_software, logica).
+afinidad(ingenieria_software, resolucion_problemas).
+afinidad(ingenieria_software, tecnologia).
+afinidad(ingenieria_software, pensamiento_abstracto).
+afinidad(ingenieria_software, creatividad).
+afinidad(ingenieria_software, trabajo_autonomo).
+
+afinidad(derecho, lectura).
+afinidad(derecho, argumentacion).
+afinidad(derecho, justicia_social).
+afinidad(derecho, memoria).
+afinidad(derecho, oratoria).
+afinidad(derecho, pensamiento_critico).
+afinidad(derecho, escritura).
+
+afinidad(psicologia, empatia).
+afinidad(psicologia, escucha_activa).
+afinidad(psicologia, ciencias_sociales).
+afinidad(psicologia, curiosidad_humana).
+afinidad(psicologia, paciencia).
+afinidad(psicologia, ayudar_personas).
+afinidad(psicologia, observacion).
+
+afinidad(arquitectura, matematicas).
+afinidad(arquitectura, creatividad).
+afinidad(arquitectura, dibujo).
+afinidad(arquitectura, arte).
+afinidad(arquitectura, diseno).
+afinidad(arquitectura, vision_espacial).
+afinidad(arquitectura, planificacion).
+
+afinidad(administracion_empresas, liderazgo).
+afinidad(administracion_empresas, organizacion).
+afinidad(administracion_empresas, matematicas).
+afinidad(administracion_empresas, comunicacion).
+afinidad(administracion_empresas, toma_decisiones).
+afinidad(administracion_empresas, trabajo_en_equipo).
+afinidad(administracion_empresas, economia).
+
+afinidad(ensenanza, paciencia).
+afinidad(ensenanza, comunicacion).
+afinidad(ensenanza, vocacion_servicio).
+afinidad(ensenanza, creatividad).
+afinidad(ensenanza, empatia).
+afinidad(ensenanza, trabajo_en_equipo).
+afinidad(ensenanza, organizacion).
+
+afinidad(periodismo, escritura).
+afinidad(periodismo, curiosidad).
+afinidad(periodismo, comunicacion).
+afinidad(periodismo, pensamiento_critico).
+afinidad(periodismo, investigacion).
+afinidad(periodismo, oratoria).
+afinidad(periodismo, adaptabilidad).
+
+afinidad(diseno_grafico, creatividad).
+afinidad(diseno_grafico, arte).
+afinidad(diseno_grafico, tecnologia).
+afinidad(diseno_grafico, comunicacion_visual).
+afinidad(diseno_grafico, dibujo).
+afinidad(diseno_grafico, estetica).
+afinidad(diseno_grafico, trabajo_autonomo).
+
+afinidad(biologia, ciencias_naturales).
+afinidad(biologia, investigacion).
+afinidad(biologia, medio_ambiente).
+afinidad(biologia, laboratorio).
+afinidad(biologia, observacion).
+afinidad(biologia, pensamiento_analitico).
+afinidad(biologia, paciencia).
+
+afinidad(contabilidad, matematicas).
+afinidad(contabilidad, orden).
+afinidad(contabilidad, organizacion).
+afinidad(contabilidad, economia).
+afinidad(contabilidad, atencion_al_detalle).
+afinidad(contabilidad, trabajo_autonomo).
+afinidad(contabilidad, planificacion).
+
+afinidad(trabajo_social, empatia).
+afinidad(trabajo_social, justicia_social).
+afinidad(trabajo_social, vocacion_servicio).
+afinidad(trabajo_social, comunicacion).
+afinidad(trabajo_social, paciencia).
+afinidad(trabajo_social, ayudar_personas).
+afinidad(trabajo_social, trabajo_en_equipo).
+
+antagonia(medicina, tecnologia).
+antagonia(medicina, trabajo_autonomo).
+antagonia(medicina, aversion_sangre).
+antagonia(medicina, poca_tolerancia_presion).
+
+antagonia(ingenieria_software, oratoria).
+antagonia(ingenieria_software, trabajo_en_equipo_intenso).
+antagonia(ingenieria_software, ciencias_sociales).
+antagonia(ingenieria_software, artes_escena).
+
+antagonia(derecho, matematicas).
+antagonia(derecho, laboratorio).
+antagonia(derecho, trabajo_autonomo).
+antagonia(derecho, poca_tolerancia_presion).
+
+antagonia(psicologia, matematicas).
+antagonia(psicologia, trabajo_autonomo).
+antagonia(psicologia, tecnologia).
+antagonia(psicologia, poca_paciencia).
+
+antagonia(arquitectura, ciencias_sociales).
+antagonia(arquitectura, poca_atencion_al_detalle).
+antagonia(arquitectura, trabajo_en_equipo_intenso).
+antagonia(arquitectura, aversion_tecnologia).
+
+antagonia(administracion_empresas, trabajo_autonomo).
+antagonia(administracion_empresas, introversion_extrema).
+antagonia(administracion_empresas, ciencias_naturales).
+antagonia(administracion_empresas, laboratorio).
+
+antagonia(ensenanza, introversion_extrema).
+antagonia(ensenanza, poca_paciencia).
+antagonia(ensenanza, trabajo_autonomo).
+antagonia(ensenanza, poca_tolerancia_presion).
+
+antagonia(periodismo, rutina).
+antagonia(periodismo, laboratorio).
+antagonia(periodismo, matematicas).
+antagonia(periodismo, introversion_extrema).
+
+antagonia(diseno_grafico, matematicas).
+antagonia(diseno_grafico, ciencias_naturales).
+antagonia(diseno_grafico, trabajo_en_equipo_intenso).
+antagonia(diseno_grafico, poca_creatividad).
+
+antagonia(biologia, matematicas_avanzadas).
+antagonia(biologia, tecnologia).
+antagonia(biologia, oratoria).
+antagonia(biologia, trabajo_en_equipo_intenso).
+
+antagonia(contabilidad, creatividad_espontanea).
+antagonia(contabilidad, ciencias_sociales).
+antagonia(contabilidad, oratoria).
+antagonia(contabilidad, trabajo_en_equipo_intenso).
+
+antagonia(trabajo_social, trabajo_autonomo).
+antagonia(trabajo_social, matematicas).
+antagonia(trabajo_social, tecnologia).
+antagonia(trabajo_social, poca_empatia).
