6590c607120e28e84b44a9352690b18954dfca5d
ShadowGlimmer <>
2026-05-03 00:32:04 -0600

    Se modifica el BNF

--- BNF.pl
+++ BNF.pl
@@ -53,31 +53,39 @@ complemento --> preposicion, pronombre.
 afirmacion --> [si] | [claro] | [obvio] | [exacto] | [correcto].
 negacion --> [no] | [nunca] | [jamas] | [tampoco].
 
-determinante --> [el] | [la] | [los] | [las] | [un] | [una] | [esas].
-sustantivo --> [cosas] | [juego] | [moto] | [anime] | [proyecto] | [idea] | [atencion].
+determinante --> [el] | [la] | [los] | [las] | [un] | [una] | [uno]| [unos]| [unas]| [esas].
+sustantivo --> [cosas] | [juego] | [moto] | [anime] | [proyecto] | [idea] | [atencion] | [aire]| [libro] | [libros] | [felicidad]
+|[materia]| [numeros]| [calculo]| [ecuaciones]| [geometria]|[problemas]|[resolucion]| [software]|[programacion]| [estrategia]| [comunicacion]| [interaccion] | [empatia]|
+ [grupo]| [conversacion]| [dialogo]| [lectura] | [literatura]| [texto]| [expresion]| [compromiso]| [paisaje]| [cesped]| [tierra]| 
+ [audiencia]| [voz]| [discurso]| [presentacion]| [presentaciones]| [argumento]| [argumentos]| [personas].
 
-pronombre --> [yo] | [nosotros] | [el] | [ella] | [mi].
-pronombre_objeto --> [me] | [te] | [se] | [nos] | [lo] | [la].
+pronombre --> [yo] | [nosotros] |[nosotras]| [el] | [ella] | [ellos] | [ellas] | [mi].
+pronombre_objeto --> [me] | [te] | [se] | [nos] | [lo] | [la] |[le]| [los] | [las].
 
-preposicion --> [de] | [por] | [en] | [a] | [con].
+preposicion --> [de] | [por] | [en] | [a] | [con] | [para].
 adverbio --> [asi] | [bien] | [mas].
 
-adjetivo --> [interesante] | [chiva] | [genial].
-adjetivo_negativo --> [aburrido] | [dificil] | [aburridas] | [dificiles].
+adjetivo --> [interesante] | [chiva] | [genial]| [feliz]| [entretenido]| [ameno]| [increible].
+adjetivo_negativo --> [aburrido] | [dificil] | [aburridas] | [dificiles]| [complicado]| [complicados].
 
 verbo_positivo --> [amo] | [amas] | [amamos] | [aman] | [amaba].
 verbo_positivo --> [gusta] | [gustan] | [gustaba] | [gustaban] | [gustaria].
+verbo_positivo --> [agrada] | [agradan] | [agradaba] | [agradaban] | [agradaria].
 verbo_positivo --> [encanta] | [encantan] | [encantaba] | [encantaria].
 verbo_positivo --> [disfruto] | [disfrutamos] | [apasiona] | [apasionan].
 verbo_positivo --> [interesa] | [interesan] | [adoro].
+verbo_positivo --> [fascina] | [fascinan].
 % Agregados para suplir las frases indirectas sin romper la gramática:
 verbo_positivo --> [defiendo] | [suena] | [llama] | [soy] | [prefiero] | [inclino].
 
 verbo_negativo --> [odio] | [odiamos] | [odian] | [odiaba].
 verbo_negativo --> [detesto] | [detestamos] | [detestan].
 verbo_negativo --> [desagrada] | [desagradan] | [aburre] | [aburren].
-verbo_negativo --> [tolero] | [soporto] | [aguanto].
+verbo_negativo --> [tolero] | [soporto] | [aguanto]| [tolerar] | [soportar] | [aguantar].
+verbo_negativo --> [falta] | [faltar]| [evitar]| [evito]|[excluir]|[rechazo].
 
-verbo_neutro --> [hace].
-verbo_modal --> [puede].
-verbo_infinitivo --> [ser] | [amar] | [gustar] | [encantar] | [disfrutar] | [apasionar] | [interesar] | [adorar] | [odiar] | [detestar] | [desagradar] | [aburrir] | [tolerar] | [soportar] | [aguantar].
+verbo_neutro --> [hace]| [hablo]| [leo]| [tengo].
+verbo_modal --> [puede]|[pueda]| [quiera]| [quiero]| [quiere]| [debe]| [deberia]| [suelo]| [soler].
+verbo_infinitivo --> [ser] | [amar] | [gustar] | [encantar] | [disfrutar] | [apasionar] |
+ [interesar] | [adorar] | [odiar] | [detestar] | [desagradar] | [aburrir] | [tolerar] | [soportar] | [aguantar]|
+ [hacer]| [leer]| [salir]| [caminar]| [hablar].
